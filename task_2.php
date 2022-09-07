f2195874511616a2659c6113f7c45649472da052
AntGorbunov <gorbunov@centrfenix.ru>
2022-09-07 22:20:25 +0300

    'initial'

---
+++ task_2.php
@@ -0,0 +1,21 @@
+<?php
+$massiv_1 = [];
+
+for ($i = 'A'; $i <= 'B'; $i++) {
+    for ($z = 0; $z <= 999; $z++) {
+        $z = sprintf('%03d', $z); {
+            for ($h = 'A'; $h <= 'B'; $h++) {
+                for ($p = 'A'; $p <= 'B'; $p++) {
+                    $massiv_1[] = "$i$z$h$p";
+                }
+            }
+        }
+    }
+}
+
+$massiv_2 = [];
+foreach ($massiv_1 as $key => $a) {
+    $massiv_2[] = $a;
+}
+$massiv3 = {};
+echo implode("\n", $massiv_2);
